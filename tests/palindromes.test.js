d7998ab9cec2bc22bd72419b1fcb99ca40e8dddd
Orsolya Toth-Gati <PixiePal@Orsolyas-MacBook-Pro.local>
2019-01-04 07:15:53 +0200

    Initial Prettier setup

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -2,7 +2,10 @@ const palindromes = require('../palindromes')
 
 describe('palindromes()', () => {
     it('identifies one-word palindromes', () => {
-        expect(palindromes('madam')).toEqual(['madam'])
+        expect(
+            palindromes(
+                'madam')).toEqual(
+                    ['madam'])
         expect(palindromes('holloh')).toEqual(['holloh'])
     })
 })
