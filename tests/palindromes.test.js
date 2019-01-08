6940c4fd68778a1b8310f05bdf363fe65847dc57
PixiePal <orsolya.tothgati@gmail.com>
2019-01-08 12:21:42 +0100

    Format with prettier

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -1,12 +1,8 @@
-const palindromes = require('../palindromes')
+const palindromes = require('../palindromes');
 
 describe('palindromes()', () => {
-    it('identifies one-word palindromes', () => {
-        expect(
-            palindromes(
-                'madam')).toEqual(
-                    ['madam'])
-        expect(palindromes('holloh')).toEqual(['holloh'])
-    })
-})
-
+  it('identifies one-word palindromes', () => {
+    expect(palindromes('madam')).toEqual(['madam']);
+    expect(palindromes('holloh')).toEqual(['holloh']);
+  });
+});
