c3d8394dab4daaa87964f5dd51bedcfec44b0078
PixiePal <orsolya.tothgati@gmail.com>
2019-01-09 11:19:39 +0100

    intoro wallaby and complete impl

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,53 @@
-module.exports = (str) => {
-    return [str]
-}
+const reverseString = str => {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+};
+
+const stripPunctuation = str => {
+  return str.replace(/[^a-z]/g, '');
+};
+
+const isPalindrome = str => {
+  return str === reverseString(str);
+};
+
+const palindromes = str => {
+  str = stripPunctuation(str.toLowerCase());
+
+  let results = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      let part = str.substring(i, j + 1);
+      if (part.length > 2 && isPalindrome(part)) {
+        if (results.indexOf(part) < 0) {
+          results.push(part);
+        }
+      }
+    }
+  }
+
+  let strippedResults = [];
+  results.forEach((element, index, array) => {
+    let isContained = false;
+    for (let i = 0; i < array.length; i++) {
+      if (i != index && array[i].indexOf(element) >= 0) {
+        isContained = true;
+      }
+    }
+
+    if (!isContained) {
+      strippedResults.push(element);
+    }
+  });
+
+  return strippedResults;
+};
+
+palindromes.reverseString = reverseString;
+palindromes.stripPunctuation = stripPunctuation;
+palindromes.isPalindrome = isPalindrome;
+
+module.exports = palindromes;
--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -1,8 +1,56 @@
 const palindromes = require('../palindromes');
 
+const { reverseString, stripPunctuation, isPalindrome } = palindromes;
+
+describe('reverseString()', () => {
+  it('reverses a string', () => {
+    expect(reverseString('abc')).toEqual('cba');
+  });
+});
+
+describe('stripPunctuation()', () => {
+  it('removes commas', () => {
+    expect(stripPunctuation('a,b,c')).toEqual('abc');
+  });
+  it('removes white spaces ', () => {
+    expect(stripPunctuation('   a b  c     ')).toEqual('abc');
+  });
+});
+
+describe('isPalindrome()', () => {
+  it('recognizes a true palindrome', () => {
+    expect(isPalindrome('ama')).toBeTruthy();
+  });
+  it('recognizes a false palindrome', () => {
+    expect(isPalindrome('akma')).toBeFalsy();
+  });
+});
+
 describe('palindromes()', () => {
   it('identifies one-word palindromes', () => {
     expect(palindromes('madam')).toEqual(['madam']);
-    expect(palindromes('holloh')).toEqual(['holloh']);
+    expect(palindromes('hooh')).toEqual(['hooh']);
+  });
+  it('returns an empty array when given no palindromes', () => {
+    expect(palindromes('tic tac toe')).toEqual([]);
+  });
+  it('ignores casing', () => {
+    expect(palindromes('WoW')).toEqual(['wow']);
+  });
+  it('ignores punctuation', () => {
+    expect(palindromes('yo, banana boy!')).toEqual(['yobananaboy']);
+  });
+  it('detects multi-word palindromes', () => {
+    expect(palindromes('A man, a plan, a canal, Panama')).toEqual([
+      'amanaplanacanalpanama',
+    ]);
+  });
+  it('detects multiple palindromes in a text', () => {
+    expect(palindromes('mananamullu')).toEqual(['mananam', 'ullu']);
+    expect(palindromes('mananamullupogop')).toEqual([
+      'mananam',
+      'ullu',
+      'pogop',
+    ]);
   });
 });
